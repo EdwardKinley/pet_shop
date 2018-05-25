8a244ce253108ee2115311bd3035d7b2e938835c
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-05-25 16:28:13 +0100

    all tests pass

--- pet_shop.rb
+++ pet_shop.rb
@@ -36,4 +36,48 @@ def find_pet_by_name(shop, pet_name)
   for pet in shop[:pets]
     return pet if pet[:name] == pet_name
   end
+  return nil
+end
+
+def remove_pet_by_name(shop, pet_name)
+  for pet in shop[:pets]
+    shop[:pets].delete(pet) if pet[:name] == pet_name
+  end
+end
+
+def add_pet_to_stock(shop, pet)
+  shop[:pets].push(pet)
+end
+
+def customer_cash(customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count()
+end
+
+def add_pet_to_customer(customer, pet)
+  customer[:pets].push(pet)
+end
+
+def customer_can_afford_pet(customer, pet)
+  return true if customer[:cash] >= pet[:price]
+  return false
+end
+
+def sell_pet_to_customer(shop, pet, customer)
+  if pet == nil || customer_can_afford_pet(customer, pet) == false
+    return
+  else
+    add_pet_to_customer(customer, pet)
+    remove_customer_cash(customer, pet[:price])
+    remove_pet_by_name(shop, pet[:name])
+    add_or_remove_cash(shop, pet[:price])
+    shop[:admin][:pets_sold] += 1
+  end
 end
