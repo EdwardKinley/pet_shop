88fec72abc1c5f610528644445087d91567666c0
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-05-25 14:20:00 +0100

    initial commit: first ten tests pass

---
+++ pet_shop.rb
@@ -0,0 +1,39 @@
+puts
+
+def pet_shop_name(shop)
+  return shop[:name]
+end
+
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop, amount)
+  shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, n)
+  shop[:admin][:pets_sold] += n
+end
+
+def stock_count(shop)
+  return shop[:pets].count()
+end
+
+def pets_by_breed(shop, breed)
+  pets_by_breed_array = []
+  for pet in shop[:pets]
+    pets_by_breed_array.push(pet) if pet[:breed] == breed
+  end
+  return pets_by_breed_array
+end
+
+def find_pet_by_name(shop, pet_name)
+  for pet in shop[:pets]
+    return pet if pet[:name] == pet_name
+  end
+end
